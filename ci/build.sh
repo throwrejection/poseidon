8273ee99bb2ee1efee935ef5af6168277ce5df6b
Atik <atik.mail@protonmail.com>
2021-09-28 14:14:38 +0200

    travis.

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 
 set -x
 set -e
-ganache-cli -a 9 -b 1 -h 0.0.0.0 -l 0xfffffffffff -g 0x01 -p 8545 > /dev/null &
+ganache-cli > /dev/null &
 TESTRPC_PID=$!
 trap "kill $TESTRPC_PID" EXIT INT TERM
 
