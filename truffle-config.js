bb3788790033eb7d9bcace57eedee0a9e6dbbe08
Atik <atik.mail@protonmail.com>
2021-09-28 13:34:32 +0200

    travis.

--- truffle-config.js
+++ truffle-config.js
@@ -11,6 +11,11 @@ module.exports = {
             port: 8545,
             network_id: "*"
         },
+        test: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        },
         ropsten: {
             host: "127.0.0.1",
             port: 8545,
