b64b5d3ce0ea789748c2870085f774b4f94ba424
Atik <atik.mail@protonmail.com>
2021-09-28 12:32:15 +0200

    travis.

--- truffle-config.js
+++ truffle-config.js
@@ -8,12 +8,12 @@ module.exports = {
         // ganache
         dev: {
             host: "localhost",
-            port: 8545,
+            port: 7545,
             network_id: "*"
         },
         ropsten: {
             host: "127.0.0.1",
-            port: 8545,
+            port: 7545,
             network_id: 3
         }
     },
